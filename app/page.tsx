d93fc6254707184ceb2a6503474de490565778f0
Curtis Chen <curtischen1234@gmail.com>
2026-03-09 13:40:44 -0700

    Add buttons to hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
+"use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const MobileFriendlyHero: React.FC = () => {
+  const router = useRouter();
   return (
     <div className="px-0 pt-5 my-md-5 text-center outline-none">
       <div className="container px-4">
         <h1 className="display-5 fw-bold mb-3">Custom Quizzes</h1>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4 display-5 text-muted">Create, Share, Take </p>
-          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5"></div>
+          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
+            <button
+              type="button"
+              className="btn btn-primary btn-lg px-4 me-sm-3"
+              onClick={() => {
+                router.push("/create");
+              }}
+            >
+              Create
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-secondary btn-lg px-4"
+              onClick={() => {
+                router.push("/quiz");
+              }}
+            >
+              See Other Quizzes
+            </button>
+          </div>
         </div>
       </div>
 
